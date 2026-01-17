6906f8ded21ff186c6261f858eae9faa8d052920
alex.ewetumo@gmail.com <Ewetumo Alexander>
2026-01-17 08:34:23 +0000

    ADD: packages

--- installers/archlinux/apps/xtras.sh
+++ installers/archlinux/apps/xtras.sh
@@ -3,7 +3,7 @@
 yay -S --noconfirm --needed \
   gnome-calculator gnome-keyring signal-desktop \
   obsidian-bin libreoffice obs-studio kdenlive \
-  xournalpp localsend-bin
+  xournalpp localsend-bin htop sysstat iftop
 
 # Packages known to be flaky or having key signing issues are run one-by-one
 for pkg in pinta typora spotify zoom; do
