c3624be7e0858a0193a50926adb802bb0e07d0f4
alex.ewetumo@gmail.com <Ewetumo Alexander>
2026-01-17 14:01:47 +0000

    FIX: atuin loading

--- installers/every/utilities.sh
+++ installers/every/utilities.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+source $DOTFILES/shell/functions
+
 yay -S --noconfirm --needed tree zoxide ripgrep exa fd fzf walker lazygit lazydocker btop impala bluetui fastfetch typora dropbox tailscale xournal++ steam 
+
+if no_command "atuin"; then 
+	curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+fi
